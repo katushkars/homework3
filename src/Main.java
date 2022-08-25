5dd1f9b6559ed0251c12c12897893eae6fa99117
Екатерина <katushkars@mail.ru>
2022-08-25 13:47:53 +0500

    задание 1 - выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int dog = 1;
+        byte coffee = 126;
+        short milk = 230;
+        long cat = 2309l;
+        float spring = 3.3f;
+        double winter = 3.3;
+        System.out.println("Значение пременной dog c типом int равно " +dog);
+        System.out.println("Значение пременной coffee c типом byte равно " +coffee);
+        System.out.println("Значение пременной milk c типом short равно " +milk);
+        System.out.println("Значение пременной cat c типом long равно " +cat);
+        System.out.println("Значение пременной spring c типом float равно " +spring);
+        System.out.println("Значение пременной winter c типом double равно " +winter);
     }
 }
